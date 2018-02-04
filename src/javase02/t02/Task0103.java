557f9642ec8084d24d7ee046072f5e31b58c1033
panicbrain <kama115@mail.ru>
2018-02-04 22:17:58 +0300

    Task 3 was edited

--- src/javase02/t02/Task0103.java
+++ src/javase02/t02/Task0103.java
@@ -10,8 +10,16 @@ public class Task0103 {
         double a = scanner.nextDouble();
         System.out.println("Введите конец отрезка b");
         double b = scanner.nextDouble();
+        while (b < a){
+            System.out.println("Конец отрезка должен быть больше его начала. Введите b, которое больше, чем a");
+            b = scanner.nextDouble();
+        }
         System.out.println("Введите шаг вычисления h");
         double h = scanner.nextDouble();
+        while (h <= 0) {
+            System.out.println("Шаг должен быть больше нуля");
+            h = scanner.nextDouble();
+        }
         for (double x = a; x <= b; x = x + h) {
             double tg = Math.tan(2 * x) - 3;
             System.out.printf("%-6.2f | %-11.4f%n", x, tg);
