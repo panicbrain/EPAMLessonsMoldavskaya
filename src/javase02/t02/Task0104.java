b5820458e1177f919ababfb06388a94e1e57c75d
panicbrain <kama115@mail.ru>
2018-02-06 23:01:02 +0300

    Add task 4

---
+++ src/javase02/t02/Task0104.java
@@ -0,0 +1,33 @@
+package javase02.t02;
+
+import java.util.Scanner;
+
+public class Task0104 {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Введите количество элементов в массиве n");
+        double d = scanner.nextDouble();
+        while (d - Math.floor(d) != 0 || d <= 0) {
+            System.out.println("Количество элементов должно быть целым числом больше нуля. Введите n.");
+            d = scanner.nextDouble();
+        }
+        int n = (int) d;
+        int[] a = new int[n];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            a[i] = (int) (Math.random() * 100);
+            System.out.println("a[" + i + "]=" + a[i]);
+        }
+        for (int i = 0; i < n / 2; i++) {
+            int summa = a[i] + a[n - i - 1];
+            System.out.println("Summa = " + summa);
+            if (max < summa) {
+                max = summa;
+            }
+        }
+        System.out.println("MAX = " + max);
+    }
+}
+
+
